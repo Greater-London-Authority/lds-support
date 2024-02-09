949dbf7b40b0e103f609dcaba6b8448a855d45db
Sven Latham <105503672+svenlatham-gla@users.noreply.github.com>
2024-02-09 17:08:09 +0000

    Tweaks to formatting and notes on adding users

--- scripts/bulk-users.js
+++ scripts/bulk-users.js
@@ -3,9 +3,39 @@
 var bulkUsers= (function() {
     // When you initialise me, pass in a DIV so I know where to render
     var domRoot = null;
-    var successCallback = function() {}
-
+    
     var jwt = null; // We'll use this for calls to the backend
+
+    var domContainer = (function() {
+        var domContainerRoot = document.createElement("DIV");
+        domContainerRoot.style.backgroundColor='#eeeeee';
+        domContainerRoot.style.fontSize='1.2em';
+        domContainerRoot.style.minWidth='700px';
+        domContainerRoot.style.padding='10px';
+
+        domTitle = document.createElement("H2");
+        domTitle.appendChild(document.createTextNode("Bulk User Tool"));
+        domContainerRoot.appendChild(domTitle);
+
+        
+        
+        domContainerInner = document.createElement("DIV");
+        domContainerRoot.appendChild(domContainerInner);
+        
+        function init() {
+
+        }
+
+        function attach(dom) {
+            domContainerInner.appendChild(dom);
+        }
+
+    function getDom() {
+        return domContainerRoot;
+    }
+
+       return { init: init, attach: attach, getDom: getDom } 
+    })();
 
     
 
@@ -15,6 +45,7 @@ var bulkUsers= (function() {
         domDescription.appendChild(document.createTextNode("Please select the publisher you want to work with"));
         var domPublishers = document.createElement("select");
         var domPublisherSelect = document.createElement("button");
+        var successCallback = function() {}
         
         function init() {
             // Attach ourselves as a child of domRoot
@@ -26,7 +57,7 @@ var bulkUsers= (function() {
             domCard.style.display = 'none';
 
             // Attach myself to domRoot
-            domRoot.appendChild(domCard);
+            domContainer.attach(domCard);
 
             // Wire up the selection button
             domPublisherSelect.addEventListener('click', function() {
@@ -53,9 +84,14 @@ var bulkUsers= (function() {
             
         }
 
-        function show(_callback) {
-            // Callback will be triggered when the completion action is taken
-            successCallback = _callback;
+        function setCallback(_callback) {
+            successCallback = _callback;   
+        }
+        function show() {
+            domCard.style.display = 'block';
+        }
+
+        function show() {
             domCard.style.display = 'block';
         }
 
@@ -65,28 +101,96 @@ var bulkUsers= (function() {
 
 
 
-        return { init: init, show: show, hide: hide }
+        return { init: init, show: show, hide: hide, setCallback: setCallback }
     })();
 
     
     var cardUsers = (function() {
         var domCard = null;
         var domDescription = document.createElement("p");
-        domDescription.appendChild(document.createTextNode("Please copy/paste a list of email addresses, one per line"));
+        domDescription.appendChild(document.createTextNode("Here is a list of users, one per line"));
         var domUsersInput = document.createElement("textarea");
+        domUsersInput.style.display = 'block';
+        domUsersInput.style.width='100%';
+        domUsersInput.style.height='300px';
         var domCardSelect = document.createElement("button");
+        domCardSelect.appendChild(document.createTextNode("Done"));
+        var successCallback = function() {}
         
         function init() {
             // Attach ourselves as a child of domRoot
             domCard = document.createElement('div');
             domCard.appendChild(domDescription);
             domCard.appendChild(domUsersInput);
-            domCardSelect.appendChild(document.createTextNode("Next"));
             domCard.appendChild(domCardSelect);
             domCard.style.display = 'none';
 
             // Attach myself to domRoot
-            domRoot.appendChild(domCard);
+            domContainer.attach(domCard);
+
+            // Wire up the selection button
+            domCardSelect.addEventListener('click', function() {
+                successCallback(domUsersInput.value);                
+            })
+
+            
+            
+        }
+        function setCallback(_callback) {
+            successCallback = _callback;   
+        }
+        function show() {
+            domCard.style.display = 'block';
+        }
+
+        function show() {
+            domCard.style.display = 'block';
+            // Get some sample data for now
+            fetch("//data.london.gov.uk/api/org/"+state.publisher, {'headers': {'Identity': jwt}}).then(function(resp) {
+                return resp.json();
+            }).then(function(json) {
+                let text = '';
+                for (let key in json.readonly.members) {
+                    member = json.readonly.members[key];
+                    text += member.email + "\n";
+                    
+                }
+                domUsersInput.value = text;
+                
+            });
+        }
+
+        function hide() {
+            domCard.style.display = 'none';
+        }
+
+
+
+        return { init: init, show: show, hide: hide, setCallback: setCallback }
+    })();
+
+    var cardAddUsers = (function() {
+        var domCard = null;
+        var domDescription = document.createElement("p");
+        domDescription.appendChild(document.createTextNode("Paste a list of emails, one per line."));
+        var domUsersInput = document.createElement("textarea");
+        domUsersInput.style.display = 'block';
+        domUsersInput.style.width='100%';
+        domUsersInput.style.height='300px';
+        var domCardSelect = document.createElement("button");
+        domCardSelect.appendChild(document.createTextNode("Process List"));
+        var successCallback = function() {}
+        
+        function init() {
+            // Attach ourselves as a child of domRoot
+            domCard = document.createElement('div');
+            domCard.appendChild(domDescription);
+            domCard.appendChild(domUsersInput);
+            domCard.appendChild(domCardSelect);
+            domCard.style.display = 'none';
+
+            // Attach myself to domRoot
+            domContainer.attach(domCard);
 
             // Wire up the selection button
             domCardSelect.addEventListener('click', function() {
@@ -97,16 +201,40 @@ var bulkUsers= (function() {
             
         }
 
-        function show(_callback) {
-            // Callback will be triggered when the completion action is taken
-            successCallback = _callback;
+        // Notes:
+        // GET to https://data.london.gov.uk/api/users/search?q=sven.latham%2Btest4%40london.gov.uk&domain=&offset=0&org=2bfc2654-75e6-43b7-af60-335d9f702c3b
+        // gives us a list, containing the member if found and the property member [true/false]
+
+        // Addition of existing user is a PATCH to
+        // https://data.london.gov.uk/api/org/2bfc2654-75e6-43b7-af60-335d9f702c3b
+        // with payload (e.g.)
+        // [{"op":"add","path":"/members/4535af5b-e4d6-400a-a336-dfb0af77f2ac","value":{"admin":false}}]
+
+        // New user flow is via WordPress:
+        // https://data.london.gov.uk/wp-admin/user-new.php
+        // Note WP authentication cookies needed
+        // Payload (POST) is 
+        /*
+        action: createuser
+        _wpnonce_create-user: xxxxxxxxx
+        _wp_http_referer: /wp-admin/user-new.php
+        user_login: {username}
+        email: {email}
+        role: user
+        noconfirmation: 1
+        createuser: Add New User
+        */
+
+        function setCallback(_callback) {
+            successCallback = _callback;   
+        }
+        function show() {
             domCard.style.display = 'block';
-            // Get some sample data for now
-            fetch("//data.london.gov.uk/api/org/"+state.publisher, {'headers': {'Identity': jwt}}).then(function(resp) {
-                return resp.text();
-            }).then(function(html) {
-                alert(html);
-            });
+        }
+
+        function show() {
+            domCard.style.display = 'block';
+            domUsersInput.value = '';
         }
 
         function hide() {
@@ -115,7 +243,50 @@ var bulkUsers= (function() {
 
 
 
-        return { init: init, show: show, hide: hide }
+        return { init: init, show: show, hide: hide, setCallback: setCallback }
+    })();
+
+
+    var cardActions = (function() {
+        var domCard = null;
+        var domBtnListUsers = document.createElement("button");
+        domBtnListUsers.appendChild(document.createTextNode("List Group Users"));
+        var domBtnAddUsers = document.createElement("button");
+        domBtnAddUsers.appendChild(document.createTextNode("Add Users"));
+        var successCallback = function() {}
+        function init() {
+            // Attach ourselves as a child of domRoot
+            domCard = document.createElement('div');
+            domCard.appendChild(domBtnListUsers);
+            domCard.appendChild(domBtnAddUsers);
+            domCard.style.display = 'none';
+
+            // Attach myself to domRoot
+            domContainer.attach(domCard);
+
+            // Wire up the selection button
+            domBtnListUsers.addEventListener('click', function() {
+                successCallback("listusers");                
+            })
+            domBtnAddUsers.addEventListener('click', function() {
+                successCallback("addusers");                
+            })
+        }
+
+        function setCallback(_callback) {
+            successCallback = _callback;   
+        }
+        function show() {
+            domCard.style.display = 'block';
+        }
+
+        function hide() {
+            domCard.style.display = 'none';
+        }
+
+
+
+        return { init: init, show: show, hide: hide, setCallback: setCallback }
     })();
 
 
@@ -123,6 +294,8 @@ var bulkUsers= (function() {
     
     function init(_domRoot) {
         domRoot = _domRoot;
+        domContainer.init();
+        domRoot.appendChild(domContainer.getDom());
         // We can only complete the init once initial state has been set
         // This loads from JS, so we need to hang around a bit.
         initTrigger = window.setInterval(triggerInit, 500);
@@ -145,12 +318,38 @@ var bulkUsers= (function() {
         jwt = window.__INITIAL_STATE__.wordpressJwt;
         cardPublisher.init();
         cardUsers.init();
-        cardPublisher.show(function(selection) { 
-            state.publisher = selection;
-            cardPublisher.hide();
-            cardUsers.show();
+        cardActions.init();
+        cardAddUsers.init();
+
+        cardActions.setCallback(function(selection) {
+            
+            if (selection == 'listusers') {
+                cardUsers.show();
+            } else if(selection == 'addusers') {
+                cardAddUsers.show();
+            } else {
+                alert('Invalid selection');
+                return;
+            }
+            cardActions.hide();
         });
         
+        cardPublisher.setCallback(function(selection) { 
+            state.publisher = selection;
+            cardPublisher.hide();
+            cardActions.show();
+        });
+
+        cardUsers.setCallback(function(selection) { 
+            cardUsers.hide();
+            cardActions.show();
+        });
+
+        cardAddUsers.setCallback(function(selection) { 
+            alert(selection);
+        });
+
+        cardPublisher.show();
     }
 
 
